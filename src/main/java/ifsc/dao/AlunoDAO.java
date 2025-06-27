09d1dc756ac1ad12ed0d1691dc9fcce3b275299b
Marcus <marcusvinicius.mtt@gmail.com>
2025-06-26 23:13:01 -0300

    excluindo arquvios desnecessarios

--- src/main/java/ifsc/dao/AlunoDAO.java
+++
@@ -1,8 +0,0 @@
-package ifsc.dao;
-import ifsc.model.Aluno;
-
-import java.util.Optional;
-
-public interface AlunoDAO {
-    Optional<ifsc.model.Aluno> buscarPorId(int id);
-}
--- src/main/java/ifsc/service/strategy/CalculoStrategy.java
+++
@@ -1,9 +0,0 @@
-package ifsc.service.strategy;
-import ifsc.model.AtividadeRealizada;
-
-import java.util.List;
-
-@FunctionalInterface
-public interface CalculoStrategy {
-    int calcularHorasValidadas(List<AtividadeRealizada> atividades, int cargaHorariaTotalCurso);
-}
